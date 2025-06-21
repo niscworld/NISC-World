5f5f508d9be2907300a7ac5c91141a1317e4a06d
NISC World <nisc.co.in@gmail.com>
2025-06-21 12:37:43 +0530

    V1: Fix: APi

--- src/api/MainApi.js
+++ src/api/MainApi.js
@@ -1,8 +1,12 @@
 const BASE_URL = 'https://nisc-world-backend.onrender.com';
+
+const ACCOUNTS_URL = `${BASE_URL}/accounts`
+
 const API_URL = `${BASE_URL}/api/v1`;
+const ACCOUNTS_API = `${ACCOUNTS_URL}/api`
 
 const CONTACT_ENDPOINT = `${API_URL}/sendMail`;
-const LOGIN_ENDPOINT = `${BASE_URL}/accounts/login`
+const LOGIN_ENDPOINT = `${ACCOUNTS_API}/login`
 
 
 // Export all
